c2688232b1d8fc6e3207fc0c2b808f84bc2ae1b2
ganeshroman <ganesh.roman@gmail.com>
2022-08-02 22:41:45 +0530

    initial commit

---
+++ app/src/main/java/com/example/testapplication/HomeActivity.java
@@ -0,0 +1,31 @@
+package com.example.testapplication;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class HomeActivity extends AppCompatActivity {
+
+    public static Intent createQuery(Context context, String query, String value) {
+        Intent intent = new Intent(context, HomeActivity.class);
+
+        Bundle b = new Bundle();
+        b.putString("QUERY", "" + query);
+        b.putString("VALUE", "" + value);
+        intent.putExtras(b);
+
+        return intent;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+
+    }
+
+
+}
---
+++ app/src/test/java/com/example/testapplication/LogicUnitTest.java
@@ -0,0 +1,36 @@
+package com.example.testapplication;
+
+import com.example.testapplication.models.ConvertUtil;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class LogicUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        Assert.assertEquals(4, 2 + 2);
+    }
+
+    @Test
+    public void conversion_isCorrect() {
+
+        float input = 100.0f;
+        float output;
+        float expected = 212f;
+        float delta = .1f;
+
+        ConvertUtil convert = new ConvertUtil();
+        output = convert.convertFarenheitToCelceius(input);
+        Assert.assertEquals(expected, output, delta);
+
+        // Assert.assertEquals(4, 2 + 2)
+        // Assert.assertTrue( boolean condition);
+        // Assert.assertFalse(boolean condition);
+        // Assert.assertNull( object);
+
+
+    }
+
+}
+
+
